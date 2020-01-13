7017f420a5b6a7a9f260929818b62c311b8a8c5c
Alessio Williams Gava <marveel@hotmail.it>
2020-01-13 15:37:44 +0000

    checking for a winner, not letting modify square after winner decided or if the square is already set

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null) /* , */
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
     renderSquare(i) {
@@ -26,13 +27,23 @@ class Board extends React.Component {
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares : squares});
+        if(checkWinner(squares) || squares[i]){//not letting set new square in case winner has been decided or the square is already set
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares : squares, xIsNext : !this.state.xIsNext});
         return (squares[i]);
     }
 
     render() {
-        const status = 'Next player : X';
+        const winner = checkWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner is ' + winner;
+        } else {
+            status = 'Next player :' + (this.state.xIsNext ? ' X' : ' O');
+        }
+
         return (
             /*<button className="square">
                 {this.props.value}
@@ -76,6 +87,25 @@ class Game extends React.Component {
     }
 }
 
+function checkWinner(squares){
+    const lines =  [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+    for(let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }    
+    }
+    return null;
+}
 ReactDOM.render(
     <Board />,
     document.getElementById('root')
