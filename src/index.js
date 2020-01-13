a87377d52758b0dfd837f5f04f373c4f38b40365
Alessio Williams Gava <marveel@hotmail.it>
2020-01-13 14:51:42 +0000

    state of moves is stored in the board. square components do not store any information, but only render what is passed from the parent board. They are controlled components, and once called will contact the board usign the Board.handleClick()

--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ import App from './App.js';
 import { readdirSync } from 'fs';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button className="square" onClick={() => { this.setState({value : 'X'});}}>
-                {this.state.value}
+            <button className="square" onClick={() => { this.props.onClick()}}>
+                {this.props.val}
             </button>
         );
     }
@@ -29,7 +22,14 @@ class Board extends React.Component {
         };
     }
     renderSquare(i) {
-        return <Square val={this.state.squares[i]}/>;
+        return (<Square val={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)} />);
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares : squares});
+        return (squares[i]);
     }
 
     render() {
